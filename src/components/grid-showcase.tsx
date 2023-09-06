398446bc20ee5994c580e8d1abd9b559160fa0f1
ArjunCybro <78548960+arjunkdot@users.noreply.github.com>
2023-09-06 19:25:51 +0530

    feat: add grid showcase

--- src/components/grid-showcase.tsx
+++ src/components/grid-showcase.tsx
@@ -1,30 +1,53 @@
 import { PageProps, Link } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import React from "react";
+import { ImageDataLike, IGatsbyImageData } from "gatsby-plugin-image";
 
-
-const GridShowcase = ({data} : PageProps<Queries.GridShowcaseQuery>) => {
+const GridShowcase = ({ data }: PageProps<Queries.GridShowcaseQuery>) => {
   return (
-    <ul>
+    <ul className="grid grid-cols-3 gap-6 mt-20 mb-20">
       {data.allMarkdownRemark.edges.map((edge) => {
         return (
-          <li key={edge.node?.id}>
-            <Link to={edge.node?.frontmatter?.path}>
-              {edge.node?.frontmatter?.mediaType === "video" ? (
-                <video autoPlay loop muted>
-                  <source
-                    src={edge.node.frontmatter.featureVideoURL}
-                    type="video/mp4"
-                  />
-                </video>
-              ) : (
-                <GatsbyImage
-                  image={getImage(edge.node?.frontmatter?.featuredImage?.childImageSharp?.gatsbyImageData)}
-                  alt="h"
-                />
-              )}
-              <h2>{edge.node?.frontmatter?.title}</h2>
-              <p>{edge.node?.frontmatter?.excerpt}</p>
+          <li key={edge.node?.id} className="group overflow-hidden">
+            <Link to={edge.node?.frontmatter?.path as string}>
+              <div className="relative overflow-hidden">
+                {edge.node?.frontmatter?.mediaType === "video" ? (
+                  <video
+                    autoPlay
+                    loop
+                    muted
+                    className="w-full h-full scale-105">
+                    <source
+                      src={edge.node.frontmatter.featureVideoURL as string}
+                      type="video/mp4"
+                    />
+                  </video>
+                ) : (
+                  <>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 720 900"
+                      width="720"
+                      height="900" className="block pointer-events-none w-full h-auto"></svg>
+                    <GatsbyImage
+                      className="absolute left-0 top-0 w-full h-full scale-105 ease-default duration-300 origin-center group-hover:scale-100 group-hover:ease-default group-hover:duration-300"
+                      image={
+                        getImage(
+                          edge.node?.frontmatter?.featuredImage?.childImageSharp
+                            ?.gatsbyImageData as ImageDataLike
+                        ) as IGatsbyImageData
+                      }
+                      alt={edge.node?.frontmatter?.featuredImgAlt as string}
+                    />
+                  </>
+                )}
+              </div>
+              <h2 className="uppercase text-[22px] font-semibold mt-5 text-bs-dark group-hover:underline">
+                {edge.node?.frontmatter?.title}
+              </h2>
+              <p className="text-bs-dark uppercase tracking-wide text-sm font-medium mt-2 max-w-[210px]">
+                {edge.node?.frontmatter?.excerpt}
+              </p>
             </Link>
           </li>
         );
@@ -34,4 +57,3 @@ const GridShowcase = ({data} : PageProps<Queries.GridShowcaseQuery>) => {
 };
 
 export default GridShowcase;
-
