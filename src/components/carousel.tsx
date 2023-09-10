d33b411e611b86bceb6ef80fca8c8a61a6680cdc
ArjunCybro <78548960+arjunkdot@users.noreply.github.com>
2023-09-10 16:18:01 +0530

    :construction: add cursor

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,6 +1,51 @@
-import React, { PropsWithChildren, useEffect, useRef } from "react";
+import React, {
+  PropsWithChildren,
+  useState,
+  useEffect,
+  useRef,
+  MouseEventHandler,
+} from "react";
+import { motion } from "framer-motion";
 
 const Carousel = ({ children }: PropsWithChildren) => {
+  // Custom Cursor
+  const cursorRef = useRef<HTMLDivElement>(null);
+  const carouselContainerRef = useRef<HTMLDivElement>(null);
+
+  const [cursorPosition, setCursorPosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const [cursorVariant, setCursorVariant] = useState("default");
+
+  const variants = {
+    active: {
+      x: cursorPosition.x - 60,
+      y: cursorPosition.y - 60,
+    },
+    reset: {
+      x: window.innerWidth - 240,
+      y: 120,
+    },
+  };
+
+  const resetCursor = () => setCursorVariant("reset");
+  const activateCursor = () => setCursorVariant("active");
+
+  function handleMouseMove(e: MouseEvent) {
+    const carouselRect = carouselContainerRef.current?.getBoundingClientRect();
+    if (carouselRect) {
+      // To reset cursor position before setting axes
+      cursorRef.current!.style.left = "0px";
+      cursorRef.current!.style.top = "0px";
+      setCursorPosition({
+        x: e.clientX - carouselRect?.left,
+        y: e.clientY - carouselRect?.top,
+      });
+    }
+  }
+  // Custom Scrollbar
   const carouselRef = useRef<HTMLUListElement>(null);
   const scrollbarContainerRef = useRef<HTMLDivElement>(null);
   const scrollbarRef = useRef<HTMLDivElement>(null);
@@ -18,22 +63,26 @@ const Carousel = ({ children }: PropsWithChildren) => {
   function handleScroll() {
     const scrollbarWidth = scrollbarRef.current?.offsetWidth;
     const scrollbarContainerWidth = scrollbarContainerRef.current?.offsetWidth;
-    
-    if ( scrollbarWidth && scrollbarContainerWidth) {
-        
+
+    if (scrollbarWidth && scrollbarContainerWidth) {
       let carouselLeftPos = carouselRef.current?.scrollLeft;
 
-
       const scrollLeftTheshould = scrollbarContainerWidth - scrollbarWidth;
-      
+
       if (carouselLeftPos && carouselLeftPos >= scrollLeftTheshould) {
         carouselLeftPos = scrollLeftTheshould;
       }
       scrollbarRef.current!.style.left = `${carouselLeftPos}px`;
     }
   }
+
   return (
-    <>
+    <div
+      className="relative cursor-none"
+      ref={carouselContainerRef}
+      onMouseMove={handleMouseMove}
+      onMouseEnter={activateCursor}
+      onMouseLeave={resetCursor}>
       <ul
         ref={carouselRef}
         className="flex gap-24 overflow-x-auto bs-no-scrollbar"
@@ -47,7 +96,14 @@ const Carousel = ({ children }: PropsWithChildren) => {
           ref={scrollbarRef}
           className="h-[2px] bg-bs-dark absolute top-0"></div>
       </div>
-    </>
+      <motion.div
+        variants={variants}
+        animate={cursorVariant}
+        ref={cursorRef}
+        className="bg-bs-pink absolute pointer-events-none top-1/2 right-[120px] -translate-y-[110%] z-10 inline-flex items-center justify-center w-[120px] h-[120px] rounded-full">
+        <span className="font-bold uppercase text-sm text-bs-dark">Drag</span>
+      </motion.div>
+    </div>
   );
 };
 
--- src/sections/index/hero-slider.tsx
+++ src/sections/index/hero-slider.tsx
@@ -1,62 +1,66 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 const HeroSlider = () => {
+  const [cursorPosition, setCursorPosition] = useState({
+    x: window.innerWidth / 2 - 60,
+    y: window.innerHeight / 3 - 60,
+  });
+  const [cursorVariant, setCursorVariant] = useState("default");
+  const variants = {
+    active: {
+      x: cursorPosition.x - 60,
+      y: cursorPosition.y - 60,
+    },
+    reset: {
+      x: window.innerWidth / 2 - 60,
+      y: window.innerHeight / 3 - 60,
+    },
+  };
 
-    const [cursorPosition, setCursorPosition] = React.useState({
-        x: window.innerWidth / 2 - 60,
-        y: window.innerHeight / 3 - 60,
-      });
-      const [cursorVariant, setCursorVariant] = React.useState("default");
-      const variants = {
-        active: {
-          x: cursorPosition.x - 60,
-          y: cursorPosition.y - 60,
-        },
-        reset: {
-          x: window.innerWidth / 2 - 60,
-          y: window.innerHeight / 3 - 60,
-        },
-      };
-    
-      const resetCursor = () => setCursorVariant("reset");
-      const activateCursor = () => setCursorVariant("active");
-    
-      React.useEffect(() => {
-        const cursorMove = (e: MouseEvent) => {
-          setCursorPosition({ x: e.clientX, y: e.clientY });
-        };
-        window.addEventListener("mousemove", cursorMove);
-    
-        return () => {
-          window.removeEventListener("mousemove", cursorMove);
-        };
-      }, []);
+  const cursorRef = useRef<HTMLDivElement>(null);
+
+  const resetCursor = () => setCursorVariant("reset");
+  const activateCursor = () => setCursorVariant("active");
+
+  React.useEffect(() => {
+    const cursorMove = (e: MouseEvent) => {
+      cursorRef.current!.style.left = "0px";
+      cursorRef.current!.style.top = "0px";
+      setCursorPosition({ x: e.clientX, y: e.clientY });
+    };
+    window.addEventListener("mousemove", cursorMove);
+
+    return () => {
+      window.removeEventListener("mousemove", cursorMove);
+    };
+  }, []);
 
   return (
     <div className="h-[100vh] w-full block relative">
-    <video autoPlay loop muted className="w-full h-full object-cover">
-      <source src="./videos/hero-video.mp4" type="video/mp4" />
-    </video>
-    <div
-      onMouseEnter={activateCursor}
-      onMouseLeave={resetCursor}
-      className="bs-cursor-container z-20 absolute w-full h-[calc(100vh_-_126px)] left-0 top-[126px]">
-      <motion.div
-        className="absolute z-10 top-0 left-0 uppercase font-bold text-sm"
-        variants={variants}
-        animate={cursorVariant}>
-        <span className=" bg-bs-white w-[120px] h-[120px] rounded-full flex justify-center items-center px-5 text-center">
-          Watch Reel
-        </span>
-        <span className="block text-center mt-3 text-bs-white text-sm">
-          Basic/Dept&reg;
-          <br />
-          2010-∞
-        </span>
-      </motion.div>
+      <video autoPlay loop muted className="w-full h-full object-cover">
+        <source src="./videos/hero-video.mp4" type="video/mp4" />
+      </video>
+      <div
+        onMouseEnter={activateCursor}
+        onMouseLeave={resetCursor}
+        className="bs-cursor-container z-20 absolute w-full h-[calc(100vh_-_126px)] left-0 top-[126px]">
+        <motion.div
+          ref={cursorRef}
+          className="relative z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-2/3 uppercase  inline-flex flex-col items-center justify-center font-bold text-sm"
+          variants={variants}
+          animate={cursorVariant}>
+          <span className=" bg-bs-white w-[120px] h-[120px] rounded-full flex justify-center items-center px-5 text-center">
+            Watch Reel
+          </span>
+          <span className="block text-center mt-3 text-bs-white text-sm">
+            Basic/Dept&reg;
+            <br />
+            2010-∞
+          </span>
+        </motion.div>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
 export default HeroSlider;
